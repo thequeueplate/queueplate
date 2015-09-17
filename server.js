3b642ea49ba707b045694330589076655e25727e
lindseybrown4 <lindseybrown4@gmail.com>
2015-09-17 13:56:39 -0600

    sendgrid email

--- server.js
+++ server.js
@@ -3,20 +3,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan'); 
 
 var config = require('./config');
-var sendgrid  = require('sendgrid')('config.sendgrid_API_KEY')
-
-
-var mongoose = require('mongoose'); //how you call your database
 var app = express(); 
-
-
-mongoose.connect(config.database, function(err) {
-	if(err) {
-		console.log(err);		
-	} else {
-		console.log('Connected to the database');
-	}
-}); 
 
 ///////     MIDDLEWARE     /////
 app.use(bodyParser.urlencoded({ extended: true })); //extended makes it accept videos and photos and strings etc
@@ -32,19 +19,6 @@ app.get('*', function(req, res) { //the asterisk will make every url go to index
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-var email = new sendgrid.Email({
-  to:       'lindseybrown4@gmail.com',
-  from:     'queueplate.com@gmail.com',
-  subject:  'Hello World',
-  text:     'My first email through SendGrid.'
-});
-
-sendgrid.send(email, function(err, json) {
-  if (err) { return console.error(err); }
-  console.log(json);
-});
-
-
 app.listen(config.port, function(err){
 	if(err) {
 		console.log(err);
@@ -52,5 +26,3 @@ app.listen(config.port, function(err){
 		console.log("Listening on port 3000");
 	}
 });
-
-
