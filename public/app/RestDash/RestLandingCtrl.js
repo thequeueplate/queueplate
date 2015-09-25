bfafee4cad079ad0cccf75261fb9fbe9a49d4741
Jeremy R <jeremy@devmounta.in>
2015-09-25 16:03:35 -0600

    Masta plan yo

--- public/app/RestDash/RestLandingCtrl.js
+++ public/app/RestDash/RestLandingCtrl.js
@@ -4,8 +4,6 @@ app.controller('RestLandingCtrl', function($scope,$cookies, $rootScope, register
   
   $scope.restFirst = $cookies.getObject("restFirstName");
   
-
-
   // $scope.deliveryAddress = function(delivery) {
   //   // console.log(delivery);
   //   firstName= delivery.firstName,
@@ -16,85 +14,7 @@ app.controller('RestLandingCtrl', function($scope,$cookies, $rootScope, register
   //   postalCode= delivery.postalCode
   // };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  $scope.getMenu = function() {
-    mainService.getMenu()
-      .then(function(response){
-        $scope.menu = angular.copy(response);
-        console.log($scope.menu);
-        if(!$scope.menu.Course) $scope.menu.Course = [];
-        if(!$scope.menu.name) $scope.menu.name = [];
-        if(!$scope.menu.description) $scope.menu.description = [];
-        if(!$scope.menu.price) $scope.menu.price = [];
-      })
-  }
-
-  $scope.menu = {
-    name:[],
-    description: [],
-    price: 0,
-    addCourse: ''
-  }
-
-
-  $scope.courses = [
-    {type: "appetizer"},
-    {type:"Main Course"},
-    {type:"Dessert"}
-  ]
-
-  /////////////// Course ////////////
-  $scope.addCourse = function(idx){
-    $scope.menu.Course.push({});
-  }
-  $scope.removeCourse = function(idx){
-    $scope.menu.Course.splice(idx, 1);
-  }
-
-  /////////////// Course ////////////
-  $scope.addName = function(idx){
-    $scope.menu.name.push({});
-  }
-  $scope.removeName = function(idx){
-    $scope.menu.name.splice(idx, 1);
-  }
-
-  /////////////// Course ////////////
-  $scope.addDescription = function(idx){
-    $scope.menu.description.push({});
-  }
-  $scope.removeDescription = function(idx){
-    $scope.menu.description.splice(idx, 1);
-  }
-
-  /////////////// Course ////////////
-  $scope.addPrice = function(idx){
-    $scope.menu.price.push({});
-  }
-  $scope.removePrice = function(idx){
-    $scope.menu.price.splice(idx, 1);
-  }
+ 
 
 
 })
