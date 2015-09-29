549bd6955caa609b8ac754d6db838b09fbc6aac0
lindseybrown4 <lindseybrown4@gmail.com>
2015-09-29 13:03:16 -0600

    redirects to menu now

--- public/app/customer/dashboard/dashboardCtrl.js
+++ public/app/customer/dashboard/dashboardCtrl.js
@@ -18,18 +18,6 @@ app.controller('dashboardCtrl', function($scope, $cookies, dashboardService) {
    $scope.addZip = $cookies.getObject("addZip")
    $scope.phoneNumber = $cookies.getObject("phoneNumber")
 
-["American", "Cajun", "Caribbean", "Chinese", "French", "German", "Greek", "Indian", "Italian", "Korean", "Lebanese", "Mediterranean", "Mexican", "Moroccan", "Soul", "Thai", "Turkish", "Vietnamese", "Other"]
- $scope.selected = [];
-
-      $scope.toggle = function (cuisine, list) {
-        var idx = list.indexOf(cuisine);
-        if (idx > -1) list.splice(idx, 1);
-        else list.push(cuisine);
-      };
-
-      $scope.exists = function (cuisine, list) {
-        return list.indexOf(cuisine) > -1;
-      };
 
 //   function DemoCtrl ($timeout, $q) {
 //     var self = this;
