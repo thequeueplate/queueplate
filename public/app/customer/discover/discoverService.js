84439bee8df94ddc822e6a8498f8a5bbda8f55e3
lindseybrown4 <lindseybrown4@gmail.com>
2015-09-30 11:01:36 -0600

    added files back after merge

---
+++ public/app/customer/discover/discoverService.js
@@ -0,0 +1,35 @@
+var app = angular.module('QueuePlate')
+
+app.service('discoverService', function($http, $q) {
+
+  this.restaurantsByName = function(restauraunts) {
+    var deferred = $q.defer();
+    $http({
+      method: "GET",
+      url: "/api/rests"
+    }).then(function(response) {
+      deferred.resolve(response.data)
+       })
+        return deferred.promise
+        console.log(deferred.promise)
+        console.log(response)
+  }
+
+  this.addFavoriteRestaurant = function() {
+    this.post('/api/users/:userid')
+  }
+
+  // this.getMenu = function() {
+  //  var deferred = $q.defer();
+  //  $http({
+  //    method: "GET",
+  //    url: "/api/rests/" + RID + "/Menu"
+  //  }).then(function(response) {
+  //    deferred.resolve(response.data)
+  //     })
+  //      return deferred.promise
+  //      console.log(deferred.promise)
+  //      console.log(response)
+  // }
+
+});
